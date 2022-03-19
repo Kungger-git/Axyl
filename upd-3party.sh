a34263fd5c752f45928e70ff0440ba10cb86a2ad
Kungger-git <matthewriel63@gmail.com>
2022-03-19 08:15:37 +0800

    update scripts

--- upd-3party.sh
+++ upd-3party.sh
@@ -4,4 +4,4 @@
 mv ./3rdparty-x86_64/* ../axyl-repo-3party/x86_64/
 
 # update repository, remove .old & push changes
-(cd ../axyl-repo-3party/x86_64/; repo-add axyl-repo-3party.db.tar.gz *.pkg.tar.zst; rm -rf *.old; git add .; git commit -m "Update 3rd Party Repository"; .tkn; git push -u origin master)
+(cd ../axyl-repo-3party/x86_64/; repo-add axyl-repo-3party.db.tar.gz *.pkg.tar.zst; rm -rf *.old; git add .; git commit -m "Update 3rd Party Repository"; .tkn personal; git push -u origin master)
--- upd-local.sh
+++ upd-local.sh
@@ -4,4 +4,4 @@
 mv ./local-x86_64/* ../axyl-repo/x86_64/
 
 # update repository, remove .old & push changes
-(cd ../axyl-repo/x86_64/; repo-add axyl-repo.db.tar.gz *.pkg.tar.zst; rm -rf *.old; git add .; git commit -m "Update Local Repository"; .tkn; git push -u origin master)
+(cd ../axyl-repo/x86_64/; repo-add axyl-repo.db.tar.gz *.pkg.tar.zst; rm -rf *.old; git add .; git commit -m "Update Local Repository"; .tkn personal; git push -u origin master)
