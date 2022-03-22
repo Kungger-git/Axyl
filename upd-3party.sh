7bb46e781b464ae16be68355812c3ac3c24e67b7
Kungger-git <matthewriel63@gmail.com>
2022-03-22 19:13:43 +0800

    [Update]: Remove redundant files.

--- upd-3party.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# moving packages to 3rd party repository
-mv ./3rdparty-x86_64/* ../axyl-repo-3party/x86_64/
-
-# update repository, remove .old & push changes
-(cd ../axyl-repo-3party/x86_64/; repo-add axyl-repo-3party.db.tar.gz *.pkg.tar.zst; rm -rf *.old; git add .; git commit -m "Update 3rd Party Repository"; .tkn personal; git push -u origin master)
